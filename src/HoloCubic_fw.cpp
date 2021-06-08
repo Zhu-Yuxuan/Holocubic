3632a2579e93861697f057412f740859e0f45c55
Zhu-Yuxuan <55224442+Zhu-Yuxuan@users.noreply.github.com>
2021-06-09 04:43:23 +0800

    init with first version

---
+++ src/HoloCubic_fw.cpp
@@ -0,0 +1,240 @@
+#include "display.h"
+#include "imu.h"
+#include "ambient.h"
+#include "network.h"
+#include "lv_port_indev.h"
+#include "lv_cubic_gui.h"
+#include "lv_demo_encoder.h"
+#include "lv_port_fatfs.h"
+#include "common.h"
+#include "config.h"
+#include "ESP32Time.h"
+#include <ESPmDNS.h>
+#include <WebServer.h>
+#include <WiFiMulti.h> // 当我们需要使用ESP8266开发板存储多个WiFi网络连接信息时，可以使用ESP8266WiFiMulti库来实现。
+
+int image_pos = 0;  // 记录相册播放的历史
+int clock_page = 0; // 时钟桌面的播放记录
+unsigned long pic_perMillis = 0;
+unsigned long preWeatherMillis = 0;           // 上一回更新天气时的毫秒数
+unsigned long preTimeMillis = 0;              // 上一回更新日期与时间时的毫秒数
+unsigned long weatherUpdataInterval = 900000; // 天气更新的时间间隔
+unsigned long timeUpdataInterval = 10000;     // 日期时钟更新的时间间隔
+
+/*** Component objects **7*/
+Display screen;
+IMU mpu;
+Ambient ambLight;
+Imu_Active *act_info; // 存放mpu6050返回的数据
+Network g_network;    // 网络连接
+ESP32Time g_rtc;      // 用于时间解码
+
+String unit = "c";
+String time_api = "http://api.m.taobao.com/rest/api3.do?api=mtop.common.getTimestamp";
+
+void clock_app_process(Imu_Active *act_info);
+void picture_app_process(Imu_Active *act_info);
+
+// function pointer array
+void (*p_processList[2])(Imu_Active *) = {clock_app_process, picture_app_process};
+void controller(Imu_Active *act_info); // universal controlled by imu
+int processId = 0;
+
+void UpdateWeather(void)
+{
+    Weather weather = g_network.getWeather("https://api.seniverse.com/v3/weather/now.json?key=" + g_cfg.weather_key + "&location=" + g_cfg.cityname + "&language=" + g_cfg.language + "&unit=" + unit); //如果要改城市这里也需要修改
+    Serial.println(weather.weather_code);
+    char temperature[10] = {0};
+    sprintf(temperature, "%d", weather.temperature);
+    display_weather(g_cfg.cityname.c_str(), temperature, weather.weather_code);
+}
+
+void UpdateTime_RTC(void)
+{
+    long int timestamp = g_network.getTimestamp(time_api) + TIMEZERO_OFFSIZE; //nowapi时间API
+    g_rtc.setTime(timestamp);
+    String date = g_rtc.getDate(String("%Y-%m-%d"));
+    String time = g_rtc.getTime(String("%H:%M"));
+
+    Serial.println("-------------------------------------");
+    Serial.println(date.c_str());
+    Serial.println(time.c_str());
+    Serial.println("-------------------------------------");
+    display_time(date.c_str(), time.c_str());
+}
+
+void wifi_auto_process()
+{
+    if (doDelayMillisTime(g_network.m_wifiClickInterval, &g_network.m_preWifiClickMillis, false) == true)
+    {
+        preWeatherMillis = millis() - weatherUpdataInterval;
+        preTimeMillis = millis() - timeUpdataInterval;
+        if (CONN_SUCC != g_network.end_conn_wifi())
+        {
+            // todo
+            // if (AP_ENABLE == g_network.getApStatus())
+            // {
+            //     g_network.open_ap();
+            //     g_network.start_web_config();
+            // }
+        }
+    }
+}
+
+void controller(Imu_Active *act_info)
+{
+    // processId == 1 是相册功能
+    if (UNKNOWN == act_info->active && 1 != processId)
+        return;
+
+    Serial.print("act_info->active: ");
+    Serial.println(act_info->active);
+    if (RETURN == act_info->active)
+    {
+        act_info->active = UNKNOWN;
+        act_info->isValid = 0;
+        processId = (processId + 1) % 2; // 此处的2与p_processList的长度一致
+    }
+
+    (*(p_processList[processId]))(act_info);
+
+    act_info->active = UNKNOWN;
+    act_info->isValid = 0;
+}
+
+void clock_app_process(Imu_Active *act_info)
+{
+    Serial.println("-----------> clock_app_process");
+
+    lv_scr_load_anim_t direction = LV_SCR_LOAD_ANIM_NONE;
+
+    if (TURN_RIGHT == act_info->active)
+    {
+        direction = LV_SCR_LOAD_ANIM_MOVE_RIGHT;
+        clock_page = (clock_page + 1) % 4;
+    }
+    else if (TURN_LEFT == act_info->active)
+    {
+        direction = LV_SCR_LOAD_ANIM_MOVE_LEFT;
+        // 以下等效与 clock_page = (clock_page - 1 + 3) % 4;
+        // +3为了不让数据溢出成负数，而导致取模逻辑错误
+        clock_page = (clock_page + 3) % 4;
+    }
+
+    Serial.println("clock_page");
+    Serial.println(clock_page);
+
+    if (clock_page != 2)
+        lv_scr_load_anim(scr[clock_page], direction, 500, 300, false);
+
+    if (0 == clock_page && millis() - preWeatherMillis >= weatherUpdataInterval)
+    {
+        // 更新天气
+        preWeatherMillis = millis();
+        UpdateWeather();
+    }
+
+    if (1 == clock_page && millis() - preTimeMillis >= timeUpdataInterval)
+    {
+        // 更新时钟
+        preTimeMillis = millis();
+        UpdateTime_RTC();
+    }
+
+    if (2 == clock_page)
+    {
+        // NULL后期可以是具体数据
+        display_hardware(NULL);
+    }
+
+    if (3 == clock_page)
+    {
+        // 如果STA模式连接失败 切换成ap模式
+        if (CONN_SUCC != g_network.end_conn_wifi())
+        {
+            g_network.open_ap();
+        }
+        display_setting(g_network.get_localIp().c_str(),
+                        g_network.get_softAPIP().c_str(),
+                        "Domain: holocubic",
+                        "WebServer Info.");
+        g_network.start_web_config();
+    }
+}
+
+void picture_app_process(Imu_Active *act_info)
+{
+    Serial.println("-----------> picture_app_process");
+    lv_scr_load_anim_t direction = LV_SCR_LOAD_ANIM_NONE;
+
+    // if (TURN_RIGHT == act_info->active)
+    // {
+    //     direction = LV_SCR_LOAD_ANIM_MOVE_RIGHT;
+    // }
+    // else if (TURN_LEFT == act_info->active)
+    // {
+    //     direction = LV_SCR_LOAD_ANIM_MOVE_LEFT;
+    // }
+
+    if (doDelayMillisTime(3000, &pic_perMillis, false) == true)
+    {
+        ++image_pos;
+    }
+
+    if (image_pos >= photo_file_num)
+    {
+        image_pos = 0;
+    }
+
+    display_photo(file_name_list[image_pos]);
+    lv_scr_load_anim(image, direction, 100, 0, false);
+}
+
+void setup()
+{
+    Serial.begin(115200);
+
+    /*** Init screen ***/
+    screen.init();
+    screen.setBackLight(0.99);
+
+    /*** Init IMU as input device ***/
+    lv_port_indev_init();
+    mpu.init();
+
+    /*** Init on-board RGB ***/
+    rgb.init();
+    rgb.setBrightness(0.1).setRGB(0, 122, 204);
+
+    /*** Init ambient-light sensor ***/
+    ambLight.init(ONE_TIME_H_RESOLUTION_MODE);
+
+    /*** Init micro SD-Card ***/
+    tf.init();
+    lv_fs_if_init();
+    config_read("/wifi.txt", &g_cfg);
+    tf.listDir("/image", 250);
+
+    /*** Read WiFi info in SD-Card, then scan & connect WiFi ***/
+    g_network.init(g_cfg.ssid, g_cfg.password);
+
+    display_init();
+}
+
+void loop()
+{
+    UpdateWeather();
+    act_info = mpu.update(200);
+    act_info->active = GO_FORWORD; // 让第一次能够执行刷新屏幕的判断
+    while (true)
+    {
+        screen.routine();
+        controller(act_info); // 运行当前进程
+        delay(300);
+        act_info = mpu.update(200);
+
+        rgb.setBrightness(ambLight.getLux() / 500.0);
+        wifi_auto_process();   // 任务调度
+        server.handleClient(); // 一定需要放在循环里扫描
+    }
+}
---
+++ src/lv_cubic_gui.c
@@ -0,0 +1,230 @@
+/*********************
+        INCLUDES
+ *********************/
+#include "lv_cubic_gui.h"
+#include "images.h"
+#include "lv_port_indev.h"
+#include <string.h>
+
+// 必须定义为全局或者静态
+lv_obj_t *scr[4];
+lv_obj_t *image;
+
+lv_obj_t *weather_image;
+lv_obj_t *cityname_label;
+lv_obj_t *temperature_label;
+
+lv_obj_t *time_image;
+lv_obj_t *date_label;
+lv_obj_t *time_label;
+
+lv_obj_t *cpu_temp_label;
+lv_obj_t *cpu_used_label;
+lv_obj_t *mem_used_label;
+lv_obj_t *net_upload_label;
+lv_obj_t *net_download_label;
+
+lv_obj_t *local_ip_label;
+lv_obj_t *ap_ip_label;
+lv_obj_t *domain_label; 
+lv_obj_t *title_label; 
+lv_obj_t *instru_label;
+
+lv_obj_t *photo_image;
+
+lv_font_declare(lv_font_montserrat_20);
+lv_font_declare(lv_font_montserrat_24);
+lv_font_declare(lv_font_montserrat_40);
+
+// 天气图标路径的映射关系
+void *image_map[] = {&Sunny, &Clear, "S:/weather/2.bin", "S:/weather/3.bin",
+                     &Cloudy, &PartlyCloudy, &Overcast, &ModerateSnow,
+                     &Thundershower, "S:/weather/12.bin", &LightRain,
+                     &ModerateRain, &HeavyRain, "S:/weather/19.bin",
+                     &Sleet, &SnowFlurry, &LightSnow, &HeavySnow,
+                     "S:/weather/26.bin", "S:/weather/27.bin", "S:/weather/28.bin",
+                     "S:/weather/29.bin", "S:/weather/30.bin", "S:/weather/31.bin",
+                     "S:/weather/32.bin", "S:/weather/33.bin", "S:/weather/34.bin",
+                     "S:/weather/35.bin", "S:/weather/36.bin", "S:/weather/37.bin",
+                     "S:/weather/38.bin", "S:/weather/99.bin"};
+int map_index[] = {0, 1, 2, 3, 4, 5, 5, 5, 5, 6,
+                   7, 8, 9, 10, 11, 12, 12, 12, 12, 13,
+                   14, 15, 16, 7, 17, 17, 18, 19, 20, 21,
+                   22, 23, 24, 25, 26, 27, 28, 29, 30};
+
+// static lv_group_t *g;
+// static lv_obj_t *tv;
+// static lv_obj_t *t1;
+// static lv_obj_t *t2;
+// static lv_obj_t *t3;
+static lv_style_t default_style;
+static lv_style_t label_style1;
+static lv_style_t label_style2;
+static lv_style_t label_style3;
+static lv_style_t label_style4;
+static lv_style_t label_style5;
+
+void display_init(void)
+{
+  lv_style_init(&default_style);
+  lv_style_set_bg_color(&default_style, LV_STATE_DEFAULT, LV_COLOR_BLACK);
+  lv_style_set_bg_color(&default_style, LV_STATE_PRESSED, LV_COLOR_GRAY);
+  lv_style_set_bg_color(&default_style, LV_STATE_FOCUSED, LV_COLOR_BLACK);
+  lv_style_set_bg_color(&default_style, LV_STATE_FOCUSED | LV_STATE_PRESSED, lv_color_hex(0xf88));
+  lv_obj_add_style(lv_scr_act(), LV_BTN_PART_MAIN, &default_style);
+
+  image = lv_obj_create(NULL, NULL);
+  photo_image = lv_img_create(image, NULL);
+
+  // scr[0] = lv_scr_act(); //在第一页创建
+  // 天气页初始化
+  scr[0] = lv_obj_create(NULL, NULL);
+  lv_obj_add_style(scr[0], LV_BTN_PART_MAIN, &default_style);
+  lv_style_init(&label_style1);
+  lv_style_set_text_opa(&label_style1, LV_STATE_DEFAULT, LV_OPA_COVER);
+  lv_style_set_text_color(&label_style1, LV_STATE_DEFAULT, LV_COLOR_WHITE);
+  lv_style_set_text_font(&label_style1, LV_STATE_DEFAULT, &lv_font_montserrat_24);
+  lv_style_init(&label_style2);
+  lv_style_set_text_opa(&label_style2, LV_STATE_DEFAULT, LV_OPA_COVER);
+  lv_style_set_text_color(&label_style2, LV_STATE_DEFAULT, LV_COLOR_WHITE);
+  lv_style_set_text_font(&label_style2, LV_STATE_DEFAULT, &lv_font_montserrat_24);
+  weather_image = lv_img_create(scr[0], NULL);
+  cityname_label = lv_label_create(scr[0], NULL);
+  temperature_label = lv_label_create(scr[0], NULL);
+
+  // 日期时间页初始化
+  scr[1] = lv_obj_create(NULL, NULL);
+  lv_obj_add_style(scr[1], LV_BTN_PART_MAIN, &default_style);
+  lv_style_init(&label_style3);
+  lv_style_set_text_opa(&label_style3, LV_STATE_DEFAULT, LV_OPA_COVER);
+  lv_style_set_text_color(&label_style3, LV_STATE_DEFAULT, LV_COLOR_WHITE);
+  lv_style_set_text_font(&label_style3, LV_STATE_DEFAULT, &lv_font_montserrat_40);
+  time_image = lv_img_create(scr[1], NULL);
+  date_label = lv_label_create(scr[1], NULL);
+  time_label = lv_label_create(scr[1], NULL);
+
+  // 硬件信息页初始化
+  scr[2] = lv_obj_create(NULL, NULL);
+  lv_obj_add_style(scr[2], LV_BTN_PART_MAIN, &default_style);
+  lv_style_init(&label_style4);
+  lv_style_set_text_opa(&label_style4, LV_STATE_DEFAULT, LV_OPA_COVER);
+  lv_style_set_text_color(&label_style4, LV_STATE_DEFAULT, LV_COLOR_WHITE);
+  lv_style_set_text_font(&label_style4, LV_STATE_DEFAULT, &lv_font_montserrat_20);
+  cpu_temp_label = lv_label_create(scr[2], NULL);
+  cpu_used_label = lv_label_create(scr[2], NULL);
+  mem_used_label = lv_label_create(scr[2], NULL);
+  net_upload_label = lv_label_create(scr[2], NULL);
+  net_download_label = lv_label_create(scr[2], NULL);
+
+  // 本地的ip地址
+  scr[3] = lv_obj_create(NULL, NULL);
+  lv_obj_add_style(scr[3], LV_BTN_PART_MAIN, &default_style);
+  lv_style_init(&label_style5);
+  lv_style_set_text_opa(&label_style5, LV_STATE_DEFAULT, LV_OPA_COVER);
+  lv_style_set_text_color(&label_style5, LV_STATE_DEFAULT, LV_COLOR_WHITE);
+  lv_style_set_text_font(&label_style5, LV_STATE_DEFAULT, &lv_font_montserrat_24);
+  local_ip_label = lv_label_create(scr[3], NULL);
+  ap_ip_label = lv_label_create(scr[3], NULL);
+  domain_label = lv_label_create(scr[3], NULL);
+  title_label = lv_label_create(scr[3], NULL);
+  instru_label = lv_label_create(scr[3],NULL);
+}
+
+void display_photo(const char *file_name)
+{
+  char buf[25] = {0};
+  sprintf(buf, "S:/image/%s.bin", file_name);
+  lv_img_set_src(photo_image, buf);
+  lv_obj_align(photo_image, NULL, LV_ALIGN_CENTER, 0, 0);
+}
+
+void display_weather(const char *cityname, const char *temperature, int weathercode)
+{
+  void *path = image_map[map_index[weathercode]];
+  if (weathercode < 39)
+  {
+    path = image_map[map_index[weathercode]];
+  }
+  else
+  {
+    path = image_map[31];
+  }
+  lv_img_set_src(weather_image, path);
+  lv_obj_align(weather_image, NULL, LV_ALIGN_OUT_TOP_MID, 0, 140);
+
+  lv_obj_add_style(cityname_label, LV_LABEL_PART_MAIN, &label_style1);
+  lv_label_set_text(cityname_label, cityname);
+  lv_obj_align(cityname_label, NULL, LV_ALIGN_OUT_BOTTOM_LEFT, 20, -65);
+
+  lv_obj_add_style(temperature_label, LV_LABEL_PART_MAIN, &label_style2);
+  lv_label_set_text_fmt(temperature_label, "%s°C", temperature);
+  lv_obj_align(temperature_label, cityname_label, LV_ALIGN_OUT_RIGHT_MID, 10, 0);
+}
+
+void display_time(const char *date, const char *time)
+{
+  char path[25] = {0};
+  sprintf(path, "S:/time/time.bin");
+  // lv_img_set_src(time_image, &huoj);
+  lv_img_set_src(time_image, path);
+  lv_obj_align(time_image, NULL, LV_ALIGN_OUT_TOP_MID, 0, 110);
+
+  lv_obj_add_style(date_label, LV_LABEL_PART_MAIN, &label_style3);
+  lv_label_set_text(date_label, date);
+  lv_obj_align(date_label, NULL, LV_ALIGN_OUT_BOTTOM_LEFT, 15, -95);
+
+  lv_obj_add_style(time_label, LV_LABEL_PART_MAIN, &label_style3);
+  lv_label_set_text(time_label, time);
+  lv_obj_align(time_label, NULL, LV_ALIGN_OUT_BOTTOM_LEFT, 65, -50);
+}
+
+void display_hardware(const char *info)
+{
+  lv_obj_add_style(cpu_temp_label, LV_LABEL_PART_MAIN, &label_style4);
+  lv_label_set_text_fmt(cpu_temp_label, "CPU Temp: %d °C", 0);
+  //lv_obj_set_pos(cpu_temp_label, 2, 30);
+  lv_obj_align(cpu_temp_label, NULL, LV_ALIGN_OUT_BOTTOM_LEFT, 2, 30);
+
+  lv_obj_add_style(cpu_used_label, LV_LABEL_PART_MAIN, &label_style4);
+  lv_label_set_text_fmt(cpu_used_label, "CPU Used: %d北京\%", 0);
+  //lv_obj_set_pos(cpu_used_label, 2, 60);
+  lv_obj_align(cpu_used_label, NULL, LV_ALIGN_OUT_BOTTOM_LEFT, 2, 60);
+
+  lv_obj_add_style(mem_used_label, LV_LABEL_PART_MAIN, &label_style4);
+  lv_label_set_text_fmt(mem_used_label, "Mem Used: %dMB", 0);
+  //lv_obj_set_pos(mem_used_label, 2, 90);
+  lv_obj_align(mem_used_label, NULL, LV_ALIGN_OUT_BOTTOM_LEFT, 2, 90);
+
+  lv_obj_add_style(net_upload_label, LV_LABEL_PART_MAIN, &label_style4);
+  lv_label_set_text_fmt(net_upload_label, "Net Upload: %dKB/s", 0);
+  //lv_obj_set_pos(net_upload_label, 2, 120);
+  lv_obj_align(net_upload_label, NULL, LV_ALIGN_OUT_BOTTOM_LEFT, 2, 120);
+
+  lv_obj_add_style(net_download_label, LV_LABEL_PART_MAIN, &label_style4);
+  lv_label_set_text_fmt(net_download_label, "Net Download: %dKB/s", 0);
+  //lv_obj_set_pos(net_download_label, 2, 150);
+  lv_obj_align(net_download_label, NULL, LV_ALIGN_OUT_BOTTOM_LEFT, 2, 150);
+}
+
+void display_setting(const char *info, const char *ap_ip, const char *domain, const char *title)
+{
+  lv_obj_add_style(local_ip_label, LV_LABEL_PART_MAIN, &label_style5);
+  lv_label_set_text(local_ip_label, info);
+  lv_obj_align(local_ip_label, NULL, LV_ALIGN_OUT_BOTTOM_LEFT, 5, -50);
+
+  lv_obj_add_style(ap_ip_label, LV_LABEL_PART_MAIN, &label_style5);
+  lv_label_set_text(ap_ip_label, ap_ip);
+  lv_obj_align(ap_ip_label, NULL, LV_ALIGN_OUT_BOTTOM_LEFT, 5, -80);
+
+  lv_obj_add_style(domain_label, LV_LABEL_PART_MAIN, &label_style5);
+  lv_label_set_text(domain_label, domain);
+  lv_obj_align(domain_label, NULL, LV_ALIGN_OUT_BOTTOM_LEFT, 5, -120);
+
+  // lv_obj_add_style(instru_label, LV_LABEL_PART_MAIN, &label_style5);
+  // lv_label_set_text(instru_label, "Type the domain\n in the explorer\n to access.");
+  // lv_obj_align(instru_label, NULL, LV_ALIGN_IN_BOTTOM_LEFT, 5, -110);
+
+  lv_obj_add_style(title_label, LV_LABEL_PART_MAIN, &label_style5);
+  lv_label_set_text(title_label, title);
+  lv_obj_align(title_label, NULL, LV_ALIGN_OUT_BOTTOM_LEFT, 5, -200);
+}
